380d9dae9090187d40324181b14823ade19107ff
Samuel Pollak <smpollak19@gmail.com>
2025-05-27 20:24:07 +0200

    fixed issue with corporate tax rate data

--- datasets/MEGAFRAME.py
+++ datasets/MEGAFRAME.py
@@ -8,10 +8,4 @@ unemp_intrate_df = pd.read_csv('datasets/OECD_LONGTERM_UNEMPLOYMENT_CLEANED.csv'
 pop_gdp_inf_df = pd.read_csv('datasets/pop_gdp_inf.csv')
 
 
-# rename necessary columns
-gini_df = gini_df.rename(columns={'value': 'GINI', 'date': 'TIME_PERIOD', 'country': 'Reference area'})
-
-megaframe = pd.merge(gini_df, tax_union_df, on=['Reference area', 'TIME_PERIOD'])
-megaframe = megaframe.drop(columns='Unnamed: 0')
-
-print(megaframe)
+# rename nece
