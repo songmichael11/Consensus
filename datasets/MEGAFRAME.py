d0b1befa26500644ec51a29528512e08ad4737b5
Samuel Pollak <smpollak19@gmail.com>
2025-06-03 14:27:08 +0200

    committed various changes related to data reading

--- datasets/MEGAFRAME.py
+++ datasets/MEGAFRAME.py
@@ -8,4 +8,12 @@ unemp_intrate_df = pd.read_csv('datasets/OECD_LONGTERM_UNEMPLOYMENT_CLEANED.csv'
 pop_gdp_inf_df = pd.read_csv('datasets/pop_gdp_inf.csv')
 
 
-# rename nece
+# rename necessary columns
+gini_df = gini_df.rename(columns={'value': 'GINI', 'date': 'TIME_PERIOD', 'country': 'Reference area'})
+
+megaframe = pd.merge(gini_df, tax_union_df, on=['Reference area', 'TIME_PERIOD'])
+megaframe = megaframe.drop(columns='Unnamed: 0')
+
+print(megaframe)
+# Side tasks:
+# Clean corporate income tax rate
