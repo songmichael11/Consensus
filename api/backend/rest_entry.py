15ed9b513dfce7281aa32fc0e88597824b4c9d12
Michael Song <songmichael11@gmail.com>
2025-06-06 00:34:32 +0200

    drafted prediction route

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -58,6 +58,7 @@ def create_app():
     app.register_blueprint(playground, url_prefix="/playground")
     app.register_blueprint(models, url_prefix="/models")
     app.register_blueprint(playground, url_prefix="/playground")
+    app.register_blueprint(models, url_prefix="/models")
 
     # Don't forget to return the app object
     return app
