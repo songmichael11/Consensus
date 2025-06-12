0b992906dfc3ff8be9b23c4c4c90563e70f7f145
Samuel Pollak <smpollak19@gmail.com>
2025-06-12 21:44:51 +0200

    added ytick labels to expanded posts

--- app/src/pages/05_Expanded_Post.py
+++ app/src/pages/05_Expanded_Post.py
@@ -56,7 +56,7 @@ def renderPlotlyGraph(post):
     fig = go.Figure(go.Scatter(x=data["x_values"], y=data['predictions'], mode="lines+markers"))
     fig.update_layout(title_text=f"{data['x_axis']} vs. GINI", 
                         margin_autoexpand=False,
-                        margin=dict(t=75, b=50, l=10, r=10),
+                        margin=dict(t=75, b=50, l=50, r=10),
                         height=400)
     st.plotly_chart(fig, key=f"plot{post['PostID']}")
 
